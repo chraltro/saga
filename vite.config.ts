feed7d910d24575bda095f986407267735fda78d
Christian Demant <christian@altro.no>
2025-10-14 12:33:31 +0200

    Change URL etc

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
-    return {
-      base: mode === 'production' ? '/saga/' : '/',
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
+  return {
+    base: mode === 'production' ? '/saga/' : '/',
+    server: {
+      port: 3000,
+      host: '127.0.0.1',
+    },
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
